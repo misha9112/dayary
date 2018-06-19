40aa9afee3a7fd2f0e4a57e0cc602b3f1323165f
jang donghyeok <commitnpush@gmail.com>
2018-06-19 11:33:39 +0900

    데이터베이스 주소 수정

---
+++ dayary.sql
@@ -0,0 +1,25 @@
+create user commitnpush identified by 11111111;
+
+grant dba to commitnpush;
+
+create table diary(
+    id number primary key,
+    u_id varchar2(15),
+    title varchar2(50),
+    content clob not null,
+    regdate date not null
+);
+create sequence seq_diary_id;
+
+insert into diary 
+values(seq_diary_id.nextval, 'flynn', '첫 번째 일기', 
+       '곧 점심시간이다', sysdate);
+       
+insert into diary 
+values(seq_diary_id.nextval, 'flynn', '두 번째 일기', 
+       '곧 프로젝트를 시작해야 한다', sysdate);
+
+commit;       
+       
+
+
